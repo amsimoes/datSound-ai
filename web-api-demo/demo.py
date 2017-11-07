b40d08dc7b0335364222b6acf1eb9f76fb60d44b
António <amsimoes@protonmail.com>
2017-11-07 22:02:52 +0000

    finishes demo

--- web-api-demo/demo.py
+++ web-api-demo/demo.py
@@ -4,6 +4,7 @@ import spotipy
 import sys
 import os
 import json
+import pprint
 
 import spotipy.oauth2 as oauth2
 import spotipy.util as util
@@ -59,8 +60,35 @@ def track_menu():
     choice = raw_input(" >>  ")
     exec_menu(choice, '1')
     return
+
+
+def track_info():
+	os.system('clear')
+	track = raw_input(" >> Track name: ")
+	results = spotify.search(q=track, limit=1)
+	pprint.pprint(results)
+	press_to_go_back(1)
+
+
+def track_features():
+	os.system('clear')
+	track = raw_input(" >> Track name: ")
+	results = spotify.search(q=track, limit=1)
+	track_id = results['tracks']['items'][0]['id']
+	pprint.pprint(spotify.audio_features([track_id]))
+	press_to_go_back(1)
+
+
+def track_analysis():
+	os.system('clear')
+	track = raw_input(" >> Track name: ")
+	results = spotify.search(q=track, limit=1)
+	track_id = results['tracks']['items'][0]['id']
+	pprint.pprint(spotify.audio_analysis(track_id))
+	press_to_go_back(1)
  
 
+# ARTIST MENU
 def artist_menu():
     print "ARTIST MENU\n"
     print "1. General Info"
@@ -74,6 +102,42 @@ def artist_menu():
     return
 
 
+def artist_info():
+	os.system('clear')
+	artist = raw_input(" >> Artist name: ")
+	results = spotify.search(q=artist, limit=1, type='artist')
+	pprint.pprint(results)
+	press_to_go_back(2)
+
+
+def artist_albums():
+	os.system('clear')
+	artist = raw_input(" >> Artist name: ")
+	results = spotify.search(q=artist, limit=1, type='artist')
+	artist_id = results['artists']['items'][0]['id']
+	pprint.pprint(spotify.artist_albums(artist_id, album_type='album'))
+	press_to_go_back(2)
+
+
+def artist_top_tracks():
+	os.system('clear')
+	artist = raw_input(" >> Artist name: ")
+	results = spotify.search(q=artist, limit=1, type='artist')
+	artist_id = results['artists']['items'][0]['id']
+	pprint.pprint(spotify.artist_top_tracks(artist_id, country='PT'))
+	press_to_go_back(2)
+
+
+def artist_related_artists():
+	os.system('clear')
+	artist = raw_input(" >> Artist name: ")
+	results = spotify.search(q=artist, limit=1, type='artist')
+	artist_id = results['artists']['items'][0]['id']
+	pprint.pprint(spotify.artist_related_artists(artist_id))
+	press_to_go_back(2)
+
+
+# ALBUM MENU
 def album_menu():
     print "ALBUM MENU\n"
     print "1. General Info"
@@ -85,11 +149,29 @@ def album_menu():
     return
 
 
+def album_info():
+	os.system('clear')
+	album = raw_input(" >> Album name: ")
+	results = spotify.search(q=album, limit=1, type='album')
+	pprint.pprint(results)
+	press_to_go_back(3)
+
+
+def album_tracks():
+	os.system('clear')
+	album = raw_input(" >> Album name: ")
+	results = spotify.search(q=album, limit=1, type='album')
+	album_id = results['albums']['items'][0]['id']
+	pprint.pprint(spotify.album_tracks(album_id))
+	press_to_go_back(3)
+
+
+# USER MENU
 def user_menu():
     print "USER MENU\n"
-    print "1. Top Artists and Tracks"
-    print "2. Recently Played Tracks"
-    print "3. List of Playlists"
+    print "1. Top Tracks"
+    print "2. Top Artists"
+    print "3. Recently Played Tracks"
     print "9. Back"
     print "0. Quit" 
     choice = raw_input(" >>  ")
@@ -97,11 +179,42 @@ def user_menu():
     return
 
 
+def user_top_tracks():
+	os.system('clear')
+	username = raw_input(" >> Username: ")
+	scope = 'user-top-read'
+	token = util.prompt_for_user_token(username, scope,client_id=CLIENT_ID,client_secret=CLIENT_SECRET,redirect_uri=REDIRECT_URI)
+	spotify = spotipy.Spotify(auth=token)
+	results = spotify.current_user_top_tracks()
+	pprint.pprint(results)
+	press_to_go_back(4)
+
+
+def user_top_artists():
+	os.system('clear')
+	username = raw_input(" >> Username: ")
+	scope = 'user-top-read'
+	token = util.prompt_for_user_token(username, scope,client_id=CLIENT_ID,client_secret=CLIENT_SECRET,redirect_uri=REDIRECT_URI)
+	spotify = spotipy.Spotify(auth=token)
+	results = spotify.current_user_top_artists()
+	pprint.pprint(results)
+	press_to_go_back(4)
+
+
+def user_recent_tracks():
+	os.system('clear')
+	username = raw_input(" >> Username: ")
+	scope = 'user-read-recently-played'
+	token = util.prompt_for_user_token(username, scope,client_id=CLIENT_ID,client_secret=CLIENT_SECRET,redirect_uri=REDIRECT_URI)
+	spotify = spotipy.Spotify(auth=token)
+	results = spotify.current_user_recently_played()
+	pprint.pprint(results)
+	press_to_go_back(4)
+
+
 def exec_menu(choice, menu_id):
-    print choice
     os.system('clear')
     ch = choice.lower()
-    print ch
     if ch == '':
         menu_actions['main_menu']()
     else:
@@ -111,6 +224,12 @@ def exec_menu(choice, menu_id):
             print "Invalid selection, please try again.\n"
             menu_actions[menu_id]['menu']()
     return
+
+
+def press_to_go_back(menu_id):
+	raw_input(">> Press to go back to menu")
+	os.system('clear')
+	menu_actions[str(menu_id)]['menu']()
 
 
 # Back to main menu
@@ -136,21 +255,33 @@ menu_actions = {
 	},
 	'1' : {
 		'menu': track_menu,
+		'1': track_info,
+		'2': track_features,
+		'3': track_analysis,
 		'9': back,
 		'0': exit,
 	},
 	'2' : {
 		'menu': artist_menu,
+		'1': artist_info,
+		'2': artist_albums,
+		'3': artist_top_tracks,
+		'4': artist_related_artists,
 		'9': back,
 		'0': exit,
 	},
 	'3' : {
 		'menu': album_menu,
+		'1': album_info,
+		'2': album_tracks,
 		'9': back,
 		'0': exit,
 	},
 	'4' : {
 		'menu': user_menu,
+		'1': user_top_tracks,
+		'2': user_top_artists,
+		'3': user_recent_tracks,
 		'9': back,
 		'0': exit,
 	},
